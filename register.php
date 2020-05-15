9ce92051ca0c77899f83dcc2b16fc913150be646
David Bauer <dbauer1@udayton.edu>
2020-05-15 16:42:15 -0400

    Update register file

--- register.php
+++ register.php
@@ -5,5 +5,22 @@ $REGISTER_LTI2 = array(
     "FontAwesome" => "fa-smile-o", // Icon for the tool
     "short_name" => "Emoji Ratings",
     "description" => "A simple tool for collecting feedback from users based on a scale using emojis.", // Tool description
-    "messages" => array("launch", "launch_grade")
+    "messages" => array("launch"),
+    "privacy_level" => "public",  // anonymous, name_only, public
+    "license" => "Apache",
+    "languages" => array(
+        "English",
+    ),
+    "source_url" => "https://github.com/udaytonapps/emoji-ratings",
+    // For now Tsugi tools delegate this to /lti/store
+    "placements" => array(
+        /*
+        "course_navigation", "homework_submission",
+        "course_home_submission", "editor_button",
+        "link_selection", "migration_selection", "resource_selection",
+        "tool_configuration", "user_navigation"
+        */
+    ),
+    "screen_shots" => array(
+    )
 );
